84f2eab461503dd58a9da42b8ef4c2b27f95a0c4
TheLostKing <calderonezj@gmail.com>
2017-09-17 23:43:13 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        String[] ruleBookList = {"Pathfinder", "Starfinder", "Burning Wheel", "Star Wars FFG", "Godbound"};
+
+        for (int i = 0; i < 5; i++){
+            System.out.print(ruleBookList[i] + "\n");
+        }
+    }
+}
